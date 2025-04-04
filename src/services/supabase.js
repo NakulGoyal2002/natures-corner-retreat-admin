9c710c00c163962a4e404ce7aac526964dc661e7
Nakul Goyal <nakul.21239@knit.ac.in>
2025-04-04 19:08:03 +0530

    few minor changes

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,8 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
-export const supabaseUrl = "https://krzdxeytdqfdgyigspcd.supabase.co";
+export const supabaseUrl = "https://jogepikrohdtulvavgri.supabase.co";
 const supabaseKey =
-	"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtyemR4ZXl0ZHFmZGd5aWdzcGNkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjEzMjE3MjEsImV4cCI6MjAzNjg5NzcyMX0.kl7IDcDaKJa37dagBu-YdoZJvkNnLUMBqI2pvWJEMxo";
+	"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpvZ2VwaWtyb2hkdHVsdmF2Z3JpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDM1OTk1ODEsImV4cCI6MjA1OTE3NTU4MX0.8w7EXtOafUQxWP9BGNJEv05JH3D4f3__qOgOZ9JEetI";
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
